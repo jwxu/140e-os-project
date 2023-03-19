9d28fc9dd445109947fb96aa7a6ed5a66f7dffef
Julia Xu <juliaxu21@gmail.com>
2023-03-19 16:13:25 -0700

    working basic sketch etch game

--- src/game.c
+++ src/game.c
@@ -18,12 +18,12 @@ struct GameObject
 
 enum {
     BLOCK_TYPE = 0,
-    BALL_TYPE = 1,
+    PEN_TYPE = 1,
     WIDTH = 800,
     HEIGHT = 480
 };
 
-struct GameObject *game_ball;
+struct GameObject *game_pen;
 
 void init_blocks() {
     color_t colors[15] = {RED, GREEN, RED, ORANGE, YELLOW, GREEN, ORANGE, BLUE, YELLOW, RED, BLUE, ORANGE, YELLOW, GREEN, BLUE};
@@ -52,52 +52,50 @@ void init_blocks() {
 
 }
 
-void init_ball() {
+void init_pen(color_t color) {
     int x_start = WIDTH/2;
-    int y_start = HEIGHT - 50;
-    int radius = 5;
-    draw_circle(x_start, y_start, radius, WHITE, 1);
+    int y_start = HEIGHT/2;
+    int pen_width = 5;
+    int pen_height = 5;
+    draw_rectangle(x_start, y_start, x_start + pen_width, y_start + pen_height, color, 1);
+    // int radius = 5;
+    // draw_circle(x_start, y_start, radius, WHITE, 1);
 
-    game_ball->x = x_start;
-    game_ball->y = y_start;
-    game_ball->radius = radius;
-    game_ball->type = BALL_TYPE;
-    game_ball->active = 1;
+    game_pen->x = x_start;
+    game_pen->y = y_start;
+    game_pen->width = pen_width;
+    game_pen->height = pen_height;
+    // game_pen->radius = radius;
+    game_pen->type = PEN_TYPE;
+    game_pen->active = 1;
 }
 
-void ball_move(int x_shift, int y_shift) {
-    // draw_rectangle(game_ball->x - game_ball->radius, game_ball->y - game_ball->radius, 
-    //             game_ball->x + game_ball->radius, game_ball->y - game_ball->radius, ORANGE, 1);
-    // // int old_x = game_ball->x;
-    // // int old_y = game_ball->y;
-    // int new_x = game_ball->x + x_shift;
-    // int new_y = game_ball->y + y_shift;
-    // if (new_x < 0 || new_x > WIDTH || new_y < 0 || new_y > HEIGHT) {
-    //     output("UH OH");
-    //     init_ball();
-    // } else {
-    //     // output("MOVE BALL");
-    //     // draw_circle(new_x, new_y, game_ball->radius, WHITE, 1);
-    //     // delay_us(10);
-    //     // draw_circle(new_x - x_shift, new_y - y_shift, game_ball->radius, ORANGE, 1);
-        
-    //     game_ball->x = new_x;
-    //     game_ball->y = new_y;
-    //     // output("X1: %d\n", game_ball->x - game_ball->radius);
-    //     // output("X2: %d\n", game_ball->x + game_ball->radius);
-    //     // output("Y1: %d\n", game_ball->y - game_ball->radius);
-    //     // output("Y2: %d\n", game_ball->y + game_ball->radius);
-    // }
+void pen_move(int x_shift, int y_shift) {
+    int new_x = game_pen->x + x_shift;
+    int new_y = game_pen->y + y_shift;
+    if (new_x < 0 || new_x > WIDTH || new_y < 0 || new_y > HEIGHT) {
+        panic();
+    } else {
+        draw_rectangle(new_x, new_y, new_x + game_pen->width, new_y + game_pen->height, WHITE, 1);
+        // draw_circle(new_x, new_y, game_pen->radius, WHITE, 1);
+        game_pen->x = new_x;
+        game_pen->y = new_y;
+    }
 }
 
-void init_game() {
-    init_blocks();
-    init_ball();
+void init_game(color_t pen_color, int obstacles) {
+    char *title = "Sketch-Etch";
+    draw_string(WIDTH/2 - (4 * FONT_WIDTH), 0, title, YELLOW);
+
+    if (obstacles == 1) {
+        init_blocks();
+    }
+    init_pen(pen_color);
 }
 
 void restart() {
     draw_solid(BLACK);
-    init_game();
+    init_game(WHITE, 0);
 }
 
 void notmain(void) {
@@ -125,34 +123,39 @@ void notmain(void) {
     // uint32_t npixels = 4;
     // neo_t neopix = neopix_init(pix_pin, npixels);
 
-    char *title = "Tilt-It";
-    int ball_x_shift = 0;
-    int ball_y_shift = 0;
+    int pen_x_shift = 0;
+    int pen_y_shift = 0;
+    int shift_amount = 4;
+
+    init_game(WHITE, 0);
+
     while (1) {
-        draw_string(WIDTH/2 - (4 * FONT_WIDTH), 0, title, YELLOW);
-
-        init_game();
-
         imu_xyz_t xyz_raw_a = accel_rd(&accel);
         imu_xyz_t xyz_raw_g = gyro_rd(&gyro);
         int accel_x = mg_scaled(xyz_raw_a.x, accel.g);
         int accel_y = mg_scaled(xyz_raw_a.y, accel.g);
         if (accel_x > 0.1) {
-            output("Tilting left\n");
+            // output("Tilting left\n");
+            pen_x_shift = -shift_amount;
         } 
         if (accel_x < -0.1) {
-            output("Tilting right\n");
+            // output("Tilting right\n");
+            pen_x_shift = shift_amount;
         } 
         if (accel_y > 0.1) {
-            output("Tilting up\n");
+            // output("Tilting up\n");
+            pen_y_shift = shift_amount;
         } 
         if (accel_y < -0.1) {
-            output("Tilting down\n");
+            // output("Tilting down\n");
+            pen_y_shift = -shift_amount;
         }
+
+        pen_move(pen_x_shift, pen_y_shift);
 
         // neopix_write(neopix, 0, RED);
         // neopix_flush(neopix);
 
-        delay_ms(1000);
+        delay_ms(500);
     }
 }
